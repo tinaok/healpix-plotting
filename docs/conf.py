cccbca90f64f6070fbed86e211473f113dd710eb
camille gueguen <253096169+cgueguen@users.noreply.github.com>
2026-03-04 10:25:53 +0100

    configuration files for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,8 @@
+
 # -- Project information -----------------------------------------------------
 import datetime as dt
+import pathlib
+import subprocess
 
 project = "healpix-plotting"
 author = f"{project} developers"
@@ -16,10 +19,16 @@ root_doc = "index"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
     "sphinx.ext.extlinks",
+    "sphinx.ext.viewcode",
+    "sphinx_design",
     "sphinx.ext.intersphinx",
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
+    "myst_nb",
 ]
 
 extlinks = {
@@ -27,26 +36,55 @@ extlinks = {
     "pull": ("https://github.com/EOPF-DGGS/healpix-plotting/pull/%s", "PR%s"),
 }
 
+myst_enable_extensions = [
+    "dollarmath",
+    "colon_fence",
+]
+
+nb_execution_mode = "auto"
+
+# Cache execution results to speed up rebuilds
+nb_execution_cache_path = "_build/.jupyter_cache"
+
+# Raise error if notebook execution fails
+nb_execution_raise_on_error = True
+
+# Execution timeout in seconds
+nb_execution_timeout = 120
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "directory"]
+exclude_patterns = ["_build", "directory", ".ipynb_checkpoints"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = "pydata-sphinx-theme"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
+
+html_css_files = ["css/custom.css"]
+
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/EOPF-DGGS/healpix-plotting",
+            "icon": "fa brands fa-square-github",
+            "type": "fontawesome",
+        },
+    ],
+    "icon_links_label": "Quick Links",
+}
 
 # -- Options for the intersphinx extension -----------------------------------
 
@@ -54,3 +92,14 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "sphinx": ("https://www.sphinx-doc.org/en/stable/", None),
 }
+
+autosummary_generate = True
+autodoc_typehints = "none"
+
+napoleon_numpy_docstring = True
+napoleon_use_param = False
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+
+
+
